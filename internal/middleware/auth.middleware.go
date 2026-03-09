66e223c5a02ebafc90584c290eada5af800865cc
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-09 11:36:50 +0700

    feat: add new middleware for auth

---
+++ internal/middleware/auth.middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/virgiIw/koda-b6-coffeshopdb/internal/lib"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		// header harus diawali dengan text bearer
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"success": false, "message": "unauthorized"})
+			ctx.Abort()
+			return
+		}
+		// kembalikan token tanpa text bearer
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, valid := lib.VerifyToken(token)
+		if !valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"success": false, "message": "unauthorized"})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("userID", claims.Id)
+		ctx.Next()
+	}
+}
