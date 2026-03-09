223dfd47565e901e069ddde5d864ae686105a3d9
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-09 11:34:51 +0700

    feat: add new dto for response error

--- internal/dto/response.dto.go
+++ internal/dto/response.dto.go
@@ -13,3 +13,9 @@ type ResponseToken struct {
 	Error   string `json:"error,omitempty"`
 	Token   string `json:"token,omitempty"`
 }
+
+type ResponseError struct {
+	Status  string `json:"status" example:"Error"`
+	Message string `json:"message" example:"Failed get data"`
+	Error   string `json:"errors,omitempty" example:"failed get data"`
+}
