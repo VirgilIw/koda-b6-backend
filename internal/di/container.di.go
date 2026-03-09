1999c045e864d02a6c4af5c1efa243e7da4e8fca
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-09 11:34:29 +0700

    feat: add function initidependencies for depedency injection repo, service, handler

--- internal/di/container.di.go
+++ internal/di/container.di.go
@@ -1,14 +1,17 @@
 package di
 
 import (
-	"github.com/jackc/pgx/v5"
-
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
 	"github.com/virgiIw/koda-b6-coffeshopdb/internal/handler"
 	"github.com/virgiIw/koda-b6-coffeshopdb/internal/repository"
 	"github.com/virgiIw/koda-b6-coffeshopdb/internal/service"
 )
 
 type Container struct {
+	db  *pgxpool.Pool
+	rdb *redis.Client
+
 	userRepo    *repository.UserRepository
 	userService *service.UserService
 	userHandler *handler.UserHandler
@@ -17,19 +20,25 @@ type Container struct {
 	authHandler *handler.AuthHandler
 }
 
-func NewContainer(db *pgx.Conn) *Container {
+func NewContainer(db *pgxpool.Pool, rdb *redis.Client) *Container {
 
-	c := &Container{}
+	container := &Container{
+		db:  db,
+		rdb: rdb,
+	}
+	container.initDependencies()
+	return container
+}
 
-	// USER
-	c.userRepo = repository.NewUserRepository(db)
+// USER
+
+func (c *Container) initDependencies() {
+	c.userRepo = repository.NewUserRepository(c.db, c.rdb)
 	c.userService = service.NewUserService(c.userRepo)
 	c.userHandler = handler.NewUserHandler(c.userService)
 
 	c.authService = service.NewAuthService(c.userRepo)
 	c.authHandler = handler.NewAuthHandler(c.authService)
-
-	return c
 }
 
 func (c *Container) UserHandler() *handler.UserHandler {
