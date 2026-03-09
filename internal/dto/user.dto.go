2ec21081413f6b79299e10e23720da743573995f
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-09 11:39:39 +0700

    feat: add pointer in users dto

--- internal/dto/user.dto.go
+++ internal/dto/user.dto.go
@@ -7,12 +7,12 @@ type UpdateUserRequest struct {
 }
 
 type Users struct {
-	Id       int    `json:"id"`
-	FullName string `json:"fullname"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Picture  string `json:"picture"`
-	Phone    string `json:"phone"`
-	Address  string `json:"address"`
-	Role     string `json:"role"`
+	Id       int     `json:"id"`
+	FullName string  `json:"fullname"`
+	Email    string  `json:"email"`
+	Password string  `json:"password"`
+	Picture  *string `json:"picture"`
+	Phone    *string `json:"phone"`
+	Address  *string `json:"address"`
+	Role     *string `json:"role"`
 }
