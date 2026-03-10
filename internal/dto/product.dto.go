99625c6f0dd3685c6d3c140181afbfe5e54b84b8
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-10 09:20:17 +0700

    feat: add detail product repo

--- internal/dto/product.dto.go
+++ internal/dto/product.dto.go
@@ -1,13 +1,18 @@
 package dto
 
+import "time"
+
 type Product struct {
-	Id                int     `json:"id"`
-	Name              string  `json:"name"`
-	Description       string  `json:"description"`
-	Price             float64 `json:"price"`
-	IsBuy1Get1        bool    `json:"is_buyget1"`
-	IsFlashSale       bool    `json:"is_flash_sale"`
-	IsBirthdayPackage bool    `json:"is_birthday_package"`
+	Id                int       `json:"id"`
+	Name              string    `json:"name"`
+	Description       string    `json:"description"`
+	Price             float64   `json:"price"`
+	IsFlashSale       bool      `json:"is_flash_sale"`
+	IsBuy1Get1        bool      `json:"is_buy1get1"`
+	IsBirthdayPackage bool      `json:"is_birthday_package"`
+	CreatedAt         time.Time `json:"created_at"`
+	ImagePath         *string   `json:"image_path"`
+	Rating            float64   `json:"rating"`
 }
 
 type ProductsResponse struct {
