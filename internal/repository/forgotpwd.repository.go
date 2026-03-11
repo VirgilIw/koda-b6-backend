3949f9618e84437eded3d9a9390f62314dd5c74d
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-11 10:30:26 +0700

    feat: add repo deletedatabycode and repo createforgotrequest

--- internal/repository/forgotpwd.repository.go
+++ internal/repository/forgotpwd.repository.go
@@ -39,3 +39,37 @@ func (f *ForgotPwdRepository) GetDataByEmailAndCode(ctx context.Context, req dto
 	return forgotPwd, nil
 
 }
+
+func (f *ForgotPwdRepository) DeleteDataByCode(ctx context.Context, req dto.ForgotPwdRequest) error {
+	query := `DELETE FROM forgot_password WHERE code_otp = $1; `
+
+	_, err := f.db.Exec(ctx, query, req.CodeOtp)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (f *ForgotPwdRepository) CreateForgotRequest(ctx context.Context, req dto.ForgotPwdRequest) (model.ForgotPassword, error) {
+	query := `
+		INSERT INTO forgot_password (email, code_otp)
+		VALUES ($1, $2)
+		RETURNING id, email, code_otp, created_at
+	`
+
+	rows, err := f.db.Query(ctx, query, req.Email, req.CodeOtp)
+
+	if err != nil {
+		return model.ForgotPassword{}, err
+	}
+
+	data, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[model.ForgotPassword])
+
+	if err != nil {
+		return model.ForgotPassword{}, err
+	}
+
+	return data, nil
+}
