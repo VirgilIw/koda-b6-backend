bd3d82e6902571b77b23b00ec644b5deb854128b
Virgil I Ambar <imanuelambar11@gmail.com>
2026-03-18 09:17:19 +0700

    feat: add new authregister service

--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
 
 	"github.com/virgiIw/koda-b6-coffeshopdb/internal/lib"
+	"github.com/virgiIw/koda-b6-coffeshopdb/internal/model"
 	"github.com/virgiIw/koda-b6-coffeshopdb/internal/repository"
 )
 
@@ -15,6 +17,11 @@ func NewAuthService(repo *repository.UserRepository) *AuthService {
 	return &AuthService{
 		repo: repo,
 	}
+}
+
+type Auth interface {
+	AuthLogin(ctx context.Context, email, password string) string
+	AuthRegister(ctx context.Context, req model.UserModel) (model.UserModel, error)
 }
 
 func (l *AuthService) AuthLogin(ctx context.Context, email, password string) string {
@@ -36,4 +43,33 @@ func (l *AuthService) AuthLogin(ctx context.Context, email, password string) str
 	}
 
 	return ""
+}
+
+func (l *AuthService) AuthRegister(ctx context.Context, req model.UserModel) (model.UserModel, error) {
+
+	user, err := l.repo.GetByEmail(ctx, req.Email)
+	if err != nil {
+		return model.UserModel{}, err
+	}
+
+	// kalau user ditemukan
+	if user.Id != 0 {
+		return model.UserModel{}, errors.New("email already registered")
+	}
+
+	hashedPassword, err := lib.HashPassword(req.Password)
+	if err != nil {
+		return model.UserModel{}, err
+	}
+
+	req.Password = hashedPassword
+
+	data, err := l.repo.CreateUser(ctx, req)
+	if err != nil {
+		return model.UserModel{}, err
+	}
+
+	data.Password = ""
+
+	return data, nil
 }
