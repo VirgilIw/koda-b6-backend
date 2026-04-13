f532abe789810551b8a18ec0bac5a120787ae275
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-13 18:07:25 +0700

    refactor: update auth dto

--- internal/dto/auth.dto.go
+++ internal/dto/auth.dto.go
@@ -6,7 +6,7 @@ type AuthLoginRequest struct {
 }
 
 type AuthRegisterRequest struct {
-	FullName string `json:"fullname" example:"John Doe"`
-	Email    string `json:"email" example:"john@email.com"`
-	Password string `json:"password" example:"password123"`
+	FullName string `json:"fullname" binding:"required,min=3,max=100" example:"John Doe"`
+	Email    string `json:"email" binding:"required,email" example:"john@email.com"`
+	Password string `json:"password" binding:"required,min=6,max=100" example:"password123"`
 }
