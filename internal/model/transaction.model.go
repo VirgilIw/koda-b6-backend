6ced747ed11d51a4b647d9805d37be60b02e4515
Virgil I Ambar <imanuelambar11@gmail.com>
2026-04-15 11:08:45 +0700

    feat: add new field in model transaction

--- internal/model/transaction.model.go
+++ internal/model/transaction.model.go
@@ -3,28 +3,32 @@ package model
 import "time"
 
 type Transaction struct {
-	ID              int        `db:"id"`
-	TransactionCode string     `db:"transaction_code"`
-	DeliveryMethod  string     `db:"delivery_method"`
-	FullName        string     `db:"full_name"`
-	Email           string     `db:"email"`
-	Address         string     `db:"address"`
-	SubtotalPrice   int        `db:"subtotal_price"`
-	TotalPrice      int        `db:"total_price"`
-	DeliveryFee     int        `db:"delivery_fee"`
-	Tax             int        `db:"tax"`
-	CouponID        *int       `db:"coupon_id"`
-	Status          string     `db:"status"`
-	PaymentMethod   string     `db:"payment_method"`
-	CreatedAt       time.Time  `db:"created_at"`
-	UpdatedAt       *time.Time `db:"updated_at"`
-	DeletedAt       *time.Time `db:"deleted_at"`
+	ID              int       `db:"id"`
+	UserID          int       `db:"user_id"`
+	TransactionCode string    `db:"transaction_code"`
+	DeliveryMethod  string    `db:"delivery_method"`
+	FullName        string    `db:"full_name"`
+	Email           string    `db:"email"`
+	Address         string    `db:"address"`
+	SubtotalPrice   int       `db:"subtotal_price"`
+	TotalPrice      int       `db:"total_price"`
+	DeliveryFee     int       `db:"delivery_fee"`
+	Tax             int       `db:"tax"`
+	Status          string    `db:"status"`
+	PaymentMethod   string    `db:"payment_method"`
+	ProductName     string    `db:"product_name"`
+	ProductImage    string    `db:"product_image"`
+	Qty             int       `db:"qty"`
+	Price           int       `db:"price"`
+	CreatedAt       time.Time `db:"created_at"`
 }
 
 type TransactionItem struct {
-	ProductID int    `db:"product_id"`
-	Qty       int    `db:"qty"`
-	Size      string `db:"size"`
-	Variant   string `db:"variant"`
-	Price     int    `db:"price"`
+	ProductID    int    `db:"product_id"`
+	ProductName  string `db:"product_name"`
+	ProductImage string `db:"product_image"`
+	Qty          int    `db:"qty"`
+	Size         string `db:"size"`
+	Variant      string `db:"variant"`
+	Price        int    `db:"price"`
 }
